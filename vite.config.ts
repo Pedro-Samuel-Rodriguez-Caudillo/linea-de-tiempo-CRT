5aa6a70c7f0a558eebc0ec212943815114cd2a2a
Pedro-Samuel-Rodriguez-Caudillo <93018448+Pedro-Samuel-Rodriguez-Caudillo@users.noreply.github.com>
2026-01-12 08:51:10 -0600

    build: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: (() => {
+    const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
+    return repo ? `/${repo}/` : '/'
+  })(),
   plugins: [react()],
 })
